7535260e9aabfe29117db2b2088a926a1a9b7fa6
Erik Karlsson <erikmatfors@gmail.com>
2024-10-18 15:43:33 +0200

    Added About Me

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AboutMeStyles.css'
+
+
+const AboutMe = () => {
+
+  return (
+    <div id='wrapper'>
+      <h2 className='landingSubheader'>About me:</h2>
+    </div>
+  )
+}
+
+export default AboutMe
