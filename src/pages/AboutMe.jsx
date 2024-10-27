1e5cfd4cacdb4468e08eb79996791ff338d936fc
Erik Karlsson <erikmatfors@gmail.com>
2024-10-27 22:24:29 +0100

    Updated AboutMe

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
     <div id='aboutmeWrapper'>
       <div className='aboutmeBox'>
       <h2 className='landingsubHeader'>ABOUT ME</h2>
-      <p>Text about me will be added here, possibly some pictures as well.</p>
+      <p>I was born in the year of the Live Aid concerts, the release of Windows 1.0 and Super Mario Bros.</p><p> Yes, you got it, 1985.</p>
     </div>
   </div>
   )
