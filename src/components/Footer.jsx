23dead8c7fc979f83c7881ea6839f83e55f389f4
Erik Karlsson <erikmatfors@gmail.com>
2024-10-27 22:03:23 +0100

    social buttons added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,10 @@ import RandomQuoteComponent from './RandomQuoteComponent'
 const Footer = () => {
   return (
     <div className='footerBox'>
-      <RandomQuoteComponent />
+      <img src="./img/instagram-128.png" width="100" height="100" alt="Instagram" />
+      <img src="./img/linkedin-128.png" width="100" height="100" alt="LinkedIn" />
+      <img src="./img/github-128.png" width="100" height="100" alt="GitHub" />
+
     </div>
   )
 }
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
   return (
     <div id='aboutmeWrapper'>
       <div className='aboutmeBox'>
-      <h2 className='landingsubHeader'>About me:</h2>
+      <h2 className='landingsubHeader'>ABOUT ME</h2>
       <p>Text about me will be added here, possibly some pictures as well.</p>
     </div>
   </div>
