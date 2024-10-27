6e1d8f9bdd4d966dedfd531c553033cced75812c
Erik Karlsson <erikmatfors@gmail.com>
2024-10-27 22:08:40 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ import RandomQuoteComponent from './RandomQuoteComponent'
 const Footer = () => {
   return (
     <div className='footerBox'>
-      <img src="./img/instagram-128.png" width="100" height="100" alt="Instagram" />
-      <img src="./img/linkedin-128.png" width="100" height="100" alt="LinkedIn" />
-      <img src="./img/github-128.png" width="100" height="100" alt="GitHub" />
+      <a href="https://www.instagram.com/frontend_erik/" target="_blank"><img src="./img/instagram-128.png" width="100" height="100" alt="Instagram" /></a>
+      <a href="https://www.linkedin.com/in/erik-karlsson7/" target="_blank"><img src="./img/linkedin-128.png" width="100" height="100" alt="LinkedIn" /></a>
+      <a href="https://github.com/Eikei7" target="_blank"><img src="./img/github-128.png" width="100" height="100" alt="GitHub" /></a>
 
     </div>
   )
