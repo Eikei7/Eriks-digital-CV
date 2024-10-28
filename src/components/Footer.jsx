76ef33aeecffd97b524cd6aff293560114048ac7
Erik Karlsson <erikmatfors@gmail.com>
2024-10-28 13:35:08 +0100

    Changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import './FooterStyles.css'
 const Footer = () => {
   return (
     <div className='footerBox'>
-      <a href="https://www.instagram.com/frontend_erik/" target="_blank"><img src="./img/instagram-128.png" width="100" height="100" alt="Instagram" /></a>
-      <a href="https://www.linkedin.com/in/erik-karlsson7/" target="_blank"><img src="./img/linkedin-128.png" width="100" height="100" alt="LinkedIn" /></a>
-      <a href="https://github.com/Eikei7" target="_blank"><img src="./img/github-128.png" width="100" height="100" alt="GitHub" /></a>
+      <a href="https://www.instagram.com/frontend_erik/" target="_blank"><img src="./img/instagram-128.png" width="40" height="40" alt="Instagram" />frontend_erik</a>
+      <a href="https://www.linkedin.com/in/erik-karlsson7/" target="_blank"><img src="./img/linkedin-128.png" width="40" height="40" alt="LinkedIn" />erik-karlsson7</a>
+      <a href="https://github.com/Eikei7" target="_blank"><img src="./img/github-128.png" width="40" height="40" alt="GitHub" />Eikei7</a>
 
     </div>
   )
