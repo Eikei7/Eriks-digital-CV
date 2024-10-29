52ca7ab329019675c970a0bf770859ea18768f58
Erik Karlsson <erikmatfors@gmail.com>
2024-10-29 11:44:17 +0100

    Colour changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,8 @@ const Contact = () => {
   return (
     <div id="wrapperContact">
       <div className='landingBox'>
-      <p className='landingText'>If you'd like to get in touch with me, send me a message using the form below and we'll take it from there!</p><br />
+      <p className='landingText'>If you'd like to get in touch, send me a message using the form below and we'll take it from there!</p>
+      <p className='emoji'>📧</p>
       <FormComponent />
       </div>
     </div>
