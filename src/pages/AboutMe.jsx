45959740daa6b520602a6616b2de4a84d84bb7ce
Erik Karlsson <erikmatfors@gmail.com>
2024-10-29 11:30:18 +0100

    Updated aboutme

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -6,7 +6,10 @@ const AboutMe = () => {
     <div id='aboutmeWrapper'>
       <div className='aboutmeBox'>
       <h2 className='landingsubHeader'>ABOUT ME</h2>
-      <p>I was born in the year of the Live Aid concerts, the release of Windows 1.0 and Super Mario Bros.</p><p> Yes, you got it, 1985.</p>
+      <p>I was born in the year when the Live Aid concerts took place, Microsoft's Windows 1.0 was released and Super Mario Bros. on the NES was available in shops.</p>
+      <p> Yes, you got it, 1985.</p>
+      <p>I chose these three events from my birth year for a reason. The reason being that, apart from growing up using Windows, I also love retro video games and music. Music is something I simply can't
+        go without. I could talk for days about The Beatles' albums or John Mayer's guitar playing, but that's not what this section was intended for.</p>
     </div>
   </div>
   )
