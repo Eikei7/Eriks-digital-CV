a64f6f93dabc751438a2be54c076331e398a2136
Erik Karlsson <erikmatfors@gmail.com>
2024-11-03 20:44:17 +0100

    More about me

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -9,8 +9,10 @@ const AboutMe = () => {
       <p>I was born in the year when the Live Aid concerts took place, Microsoft's Windows 1.0 was released and Super Mario Bros. on the NES was available in shops.</p>
       <p> Yes, it was indeed 1985.</p>
       <p>I chose these three events from my birth year for a reason. The reason being that, apart from growing up using Windows, I also love retro video games and music. Music is something I simply can't
-        go without. I could talk for days about The Beatles' albums or John Mayer's guitar playing, but that's not what this section was intended for.</p>
-        <p>So, I'm a somewhat 'seasoned' computer and Internet user. I've experienced the web since the time of dial-up modems. I was 13 years old when I built my <em>very own</em> HTML-only website about myself, complete with animated GIFs and marquee texts, hosted at Angelfire.com (remember those guys?).</p>
+        go without. I could talk for days about The Beatles' divine albums or John Mayer's incredible guitar playing, but that's not what this section was intended for.</p>
+        <p>I'd say I'm a 'seasoned' computer and Internet user. I've experienced the web since the time of dial-up modems. I was 13 years old when I built my <em>very own</em> HTML-only website about myself, complete with animated GIFs and marquee texts, hosted at Angelfire.com (remember those guys?).</p>
+        <p>Right around this time (circa 1998 - 2000) I also quickly grew an interest in computers and the technology behind them. On the hardware side for example, I learnt how graphics cards and RAM sticks worked, and practiced swapping them out for newer components on our poor family PC. Eventually I saved up for a video capture card, which allowed me to put our recorded family holiday videos onto our computer, and that's when
+          my interest in multimedia and graphics began.</p>
     </div>
   </div>
   )
