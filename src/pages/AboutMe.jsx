6ef37e3abb6862e3191cd2bf8df07e8147448289
Erik Karlsson <erikmatfors@gmail.com>
2024-11-03 19:59:27 +0100

    More about me text added

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,12 +5,13 @@ const AboutMe = () => {
   return (
     <div id='aboutmeWrapper'>
       <div className='aboutmeBox'>
-      <h2 className='landingsubHeader'>ABOUT ME</h2>
+      <h2 className='landingsubHeader'>So who's Erik then?</h2>
       <p>I was born in the year when the Live Aid concerts took place, Microsoft's Windows 1.0 was released and Super Mario Bros. on the NES was available in shops.</p>
-      <p> Yes, you got it, 1985.</p>
+      <p> Yes, it was indeed 1985.</p>
       <p>I chose these three events from my birth year for a reason. The reason being that, apart from growing up using Windows, I also love retro video games and music. Music is something I simply can't
         go without. I could talk for days about The Beatles' albums or John Mayer's guitar playing, but that's not what this section was intended for.</p>
-        <p>(More text to come)</p>
+        <p>So, I'm what some people would say, a 'seasoned' computer and Internet user. I've experienced the web since the time of dial-up modems. I was 13 years old when I built my <em>very own</em> website about myself, hosted at
+        Angelfire.com (remember those guys?).</p>
     </div>
   </div>
   )
