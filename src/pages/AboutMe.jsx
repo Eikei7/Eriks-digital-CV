d6c2064f97f89da1fe34e26a8818d40411735b51
Erik Karlsson <69363348+Eikei7@users.noreply.github.com>
2025-04-15 15:59:38 +0200

    More changes to About Me

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -10,36 +10,31 @@ const AboutMe = () => {
     {
       year: 1985,
       title: "Born in 1985",
-      image: "",
+      image: "img/windows1.png",
       content: (
-        <>
-          <p>I was born in the year when the <a href="https://en.wikipedia.org/wiki/Live_Aid" target="_blank" rel="noopener noreferrer">Live Aid concerts</a> took place, <a href="https://en.wikipedia.org/wiki/Windows_1.0" target="_blank" rel="noopener noreferrer">Microsoft's Windows 1.0</a> was released and <a href="https://en.wikipedia.org/wiki/Super_Mario_Bros." target="_blank" rel="noopener noreferrer">Super Mario Bros.</a> on the NES was available in shops.</p>
-          <p>I chose these three events from my birth year for a reason. The reason being that, apart from growing up using Windows, I also love retro video games and music.</p>
-        </>
+        <p>I was born in the year when the <a href="https://en.wikipedia.org/wiki/Live_Aid" target="_blank" rel="noopener noreferrer">Live Aid concerts</a> took place, <a href="https://en.wikipedia.org/wiki/Windows_1.0" target="_blank" rel="noopener noreferrer">Microsoft's Windows 1.0</a> was released and <a href="https://en.wikipedia.org/wiki/Super_Mario_Bros." target="_blank" rel="noopener noreferrer">Super Mario Bros.</a> on the NES was available in shops. I chose these three events from my birth year for a reason. The reason being that, apart from growing up using Windows, I also love retro video games and music.</p>
       )
     },
     {
       year: 1990,
       title: "Early Childhood",
-      image: "/img/mario-nes.jpg",
+      image: "img/mario-nes.jpg",
       content: (
-        <p>In the early 90s, I began developing my interest for video games. My sister and I shared a Nintendo 8-bit console between us, taking turns in playing Ice Climber, Zelda II, Volleyball and Super Mario bros. I found myself wondering
-          about how video games work "under the hood". </p>
+        <p>In the early 90s, I began developing my interest for video games. My sister and I shared a Nintendo 8-bit console between us, taking turns in playing Ice Climber, Zelda II, Volleyball and Super Mario bros. I found myself wondering about how video games work "under the hood".</p>
       )
     },
     {
       year: 1998,
       title: "First Website",
+      image: "img/angelfire.jpeg",
       content: (
-        <>
-          <p>I've experienced the web since the time of dial-up modems. I was 13 years old when I built my first HTML-only website about myself, complete with animated GIFs and marquee texts, hosted at Angelfire.com (remember those guys?).</p>
-          <p>Right around this time (circa 1998 - 2000) I also quickly grew an interest in computers and the technology behind them. On the hardware side for example, I learnt how graphics cards and RAM sticks worked, and practiced swapping them out for newer components on our shared family computer.</p>
-        </>
+        <p>I've experienced the web since the time of dial-up modems. I was 13 years old when I built my first HTML-only website about myself, complete with animated GIFs and marquee texts, hosted at Angelfire.com. Right around this time (circa 1998 - 2000) I also quickly grew an interest in computers and the technology behind them. On the hardware side for example, I learnt how graphics cards and RAM sticks worked, and practiced swapping them out for newer components on our shared family computer.</p>
       )
     },
     {
       year: 2000,
       title: "Digital Media Beginnings",
+      image: "img/moviemaker.png",
       content: (
         <p>Eventually I saved up for a video capture card, which allowed me to put our recorded family holiday videos onto the computer, and that's when my interest in multimedia and graphics began.</p>
       )
@@ -47,20 +42,23 @@ const AboutMe = () => {
     {
       year: 2005,
       title: "Early Career",
+      image: "img/umea.png",
       content: (
-        <p>Started working with computers professionally, gaining experience with various technologies and software systems.</p>
+        <p>I started studying to become a teacher in music and English at Umeå university in Sweden.</p>
       )
     },
     {
       year: 2010,
-      title: "Career Development",
+      title: "Teacher work",
+      image: "img/musicteacher.jpg",
       content: (
-        <p>Developed my professional skills further, working on various projects and expanding my knowledge in technology and web development.</p>
+        <p>I started my first real job as a music teacher at a school outside of Sundsvall, Sweden.</p>
       )
     },
     {
       year: 2015,
       title: "Professional Growth",
+      image: "img/windows1.png",
       content: (
         <p>By this time, I had accumulated significant experience in the tech industry, working with diverse teams and technologies.</p>
       )
@@ -68,25 +66,25 @@ const AboutMe = () => {
     {
       year: 2020,
       title: "Recent Years",
+      image: "img/nti.png",
       content: (
-        <p>Focused on frontend development, honing my skills in modern JavaScript frameworks and UI/UX design principles.</p>
+        <p>This was the year when working remotely became a reality for many people around the world. I had a position at a tech shool in Sundsvall, working as a student assistant.</p>
       )
     },
     {
       year: 2023,
-      title: "Present Day",
+      title: "Frontend studies",
+      image: "img/folkuniversitetet.jpg",
       content: (
-        <>
-          <p>Currently working as a frontend developer, continuously learning and adapting to new technologies and methodologies in the ever-evolving tech landscape.</p>
-          <p>Music remains a significant part of my life. I could talk for days about The Beatles' divine albums or John Mayer's incredible guitar playing.</p>
-        </>
+        <p>This year I began my two year path at Folkuniversitetet in Göteborg, Sweden to become a Frontend developer.</p>
       )
     },
     {
       year: 2025,
       title: "Future Goals",
+      image: "img/developer.png",
       content: (
-        <p>Looking forward to further developing my skills in web development, contributing to meaningful projects, and perhaps exploring new areas like mobile application development or AI integration.</p>
+        <p>Looking forward to graduate in June, I hope to succeed in finding a place to work where I can make use of my skills in web development, contributing to meaningful projects, and perhaps exploring new areas like AI integration.</p>
       )
     }
   ];
@@ -135,7 +133,7 @@ const AboutMe = () => {
   return (
     <div className="aboutme-container">
       <h2 className="aboutme-title">A history of Erik</h2>
-      
+            
       <div className="vertical-timeline-container">
         <div className="vertical-years-container">
           <div className="vertical-years">
@@ -166,8 +164,16 @@ const AboutMe = () => {
                   {item.title}
                 </h3>
                 <div className="vertical-event-body">
-                  {item.content}
-                  <div>{item.image}</div>
+                  <div className="vertical-event-flex">
+                    <div className="vertical-event-text">
+                      {item.content}
+                    </div>
+                    {item.image && (
+                      <div className="vertical-event-image">
+                        <img src={item.image} alt={`${item.title} illustration`} />
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
