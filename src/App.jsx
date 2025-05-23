a8e01880b03da7e38110af4a6fba746df862cae5
Erik Karlsson <69363348+Eikei7@users.noreply.github.com>
2025-05-23 15:30:03 +0200

    Added robots.txt and sitemap.xml for SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
-import './App.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import LandingPage from './pages/LandingPage'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import LandingPage from './pages/LandingPage';
+import Projects from './pages/Projects';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from "react-redux";
-import store from './store/store'
-import AboutMe from './pages/AboutMe'
+import store from './store/store';
+import AboutMe from './pages/AboutMe';
+import { Helmet } from 'react-helmet';
 
 function App() {
-
   return (
     <>
-    <Provider store={store}>
-    <Router>
-      <Navbar />
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/about' element={<AboutMe />} />
-        </Routes>
-      <Footer />
-    </Router>
-    </Provider>
+      <Helmet>
+        <title>Erik | Frontend Developer</title>
+        <meta name="description" content="Digital CV of Erik, a passionate Frontend Developer specializing in React, JavaScript, and responsive web design." />
+        <meta name="keywords" content="Frontend Developer, React Developer, Web Developer Portfolio, JavaScript Developer, UI/UX Design, Sundsvall Developer" />
+        <meta name="author" content="Erik" />
+        <link rel="canonical" href="https://frontend-erik.netlify.app/" />
+      </Helmet>
+
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/about' element={<AboutMe />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
