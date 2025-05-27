e32449281944fa9c48140122763bf424a09d9661
Erik Karlsson <69363348+Eikei7@users.noreply.github.com>
2025-05-27 15:49:25 +0200

    Fixed issue with scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,26 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from './store/store';
 import AboutMe from './pages/AboutMe';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+// ScrollToTop component
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
-    <HelmetProvider>
-      <Helmet>
-        <title>Erik Karlsson | Frontend Developer</title>
-        <meta name="description" content="Digital CV of Erik, a passionate Frontend Developer specializing in React, JavaScript, and responsive web design." />
-        <meta name="keywords" content="Frontendutvecklare, React utvecklare, webbutvecklare Portfolio, JavaScript utvecklare, UI/UX Design, Sundsvall webbutvecklare" />
-        <meta name="author" content="Erik" />
-        <link rel="canonical" href="https://frontend-erik.netlify.app/" />
-      </Helmet>
 
       <Provider store={store}>
         <Router>
+          <ScrollToTop />
           <Navbar />
           <Routes>
             <Route path='/' element={<LandingPage />} />
@@ -31,7 +36,6 @@ function App() {
           <Footer />
         </Router>
       </Provider>
-    </HelmetProvider>
   );
 }
 
